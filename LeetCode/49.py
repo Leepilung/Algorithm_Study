9c508556390e532795e3030105562d07f0727856
Lenol <ek3434@naver.com>
2021-12-09 04:01:38 +0900

    1208

---
+++ LeetCode/49.py
@@ -0,0 +1,11 @@
+# 49. Group Anagrams
+# https://leetcode.com/problems/group-anagrams/
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dic = {}
+        for i in strs:
+            key = tuple(sorted(i))  # 키값 튜플로 저장
+            dic[key] = dic.get(key, []) + [i]   # 벨류값 원형(Anagram)으로 저장
+
+        return dic.values()

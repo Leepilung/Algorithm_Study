ff88d1d555542d2a13079435a4965146ae762d4f
Lenol <ek3434@naver.com>
2021-12-09 22:56:16 +0900

    코드 리뷰 반영

--- LeetCode/125.py
+++ LeetCode/125.py
@@ -11,4 +11,8 @@ class Solution:
             if string.pop(0) != string.pop():
                 return False
         return True
-        
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i = ''.join(filter(str.isalnum, s))
+        return i.lower() == i.lower()[::-1]
--- LeetCode/5.py
+++ LeetCode/5.py
@@ -8,6 +8,7 @@ class Solution:
                 left -= 1
                 right += 1
             return s[left + 1:right]
+            # left를 만족시켰으니까 +1부터 유효, right는 인덱스까지 부분문자를 잡아야 right-1까지 출력
             
         if len(s) < 2 or s == s[::-1]:
             return s
@@ -15,6 +16,5 @@ class Solution:
         result = ''
         for i in range(len(s) -1):
             result = max(result, expand(i,i+1),expand(i,i+2),key=len)
-
-        return result
-            
+                                  # 짝수 포인터,  홀수 포인터
+        return result   

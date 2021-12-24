8313148b15111384c5991d7081f1a450933af4e4
Lenol <ek3434@naver.com>
2021-12-24 13:19:57 +0900

    스택(링크드리스트이용) 구현

---
+++ LeetCode/Implement/stack_LinkedList.py
@@ -0,0 +1,24 @@
+
+class Node:
+    def __init__(self, item, next):
+        self.item = item
+        self.next = next
+
+class Stack:
+    def __init__(self):
+        self.last = None
+    
+    def push(self, item):
+        self.last = Node(item, self.last)
+
+    def pop(self):
+        item = self.last.item
+        self.last = self.last.next
+        return item
+
+stack = Stack()
+print(stack)
+stack.push(1)
+print(stack)
+stack.push(2)
+print(stack)
